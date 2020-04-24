a0c026044c09924e4f46057c22033081edde426a
Your Name <you@example.com>
2020-04-24 17:32:21 +1000

    Init commit

---
+++ src/components/MenuItem/Content.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Content = (props) => {
+	<div className="content">
+		<h1 className="title">{props.title}</h1>
+		<span className="subtitle">{props.subtitle}</span>
+	</div>;
+};
+
+export default Content;
---
+++ src/components/MenuItem/MenuItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Content from './Content'
+function MenuItem(){
+    return(
+        <div className='menu-item'>
+            <Content />
+        </div>
+    )
+}
+
+export default MenuItem
